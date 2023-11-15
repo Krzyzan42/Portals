2604e215eea3f4904103f6d5f47c82b87c942e44
Krzyzan <jakubkrzyzanowski42@gmail.com>
2023-11-15 12:47:20 +0100

    Portal Surface now handles being scaled

--- Assets/Scripts/PortalShooting/PortalSurface.cs
+++ Assets/Scripts/PortalShooting/PortalSurface.cs
@@ -14,37 +14,13 @@ public class BoxSurfaceResult
 }
 public class PortalSurface : MonoBehaviour
 {
-	private Quaternion ForwardOrientation => Quaternion.LookRotation(transform.forward, Vector3.up);
-	private Quaternion BackwardOrientation => Quaternion.LookRotation(-transform.forward, Vector3.up);
-
 	public Vector2 initialSize;
-	private const float colliderThickness = 0.05f;
 	[Range(0f, 0.2f)]
 	public float Offset = 0.05f;
-
-	private Vector2 size;
-
-
-	public Vector2 Size {
-		get => size;
-		set {
-			size = value;
-			GetComponent<BoxCollider>().size = new Vector3(size.x, size.y, colliderThickness);
-		}
-	}
 	public Collider AttachedCollider;
 
-    public BoxSurfaceResult GetPortalPosit(Vector3 hitWorld, Vector2 portalSize) {
-		Vector3 hitLocal = transform.InverseTransformPoint(hitWorld);
-		float hitX = hitLocal.x, hitY = hitLocal.y;
-		float x = Mathf.Clamp(hitX, (-(size.x - portalSize.x))/2, (size.x - portalSize.x)/2);
-		float y = Mathf.Clamp(hitY, (-(size.y - portalSize.y)/2), (size.y - portalSize.y)/2);
-		float z = hitLocal.z > 0 ? Offset : -Offset;
-		Vector3 portalPos = transform.TransformPoint(new Vector3(x, y, z));
-		Quaternion portalOrientation = hitLocal.z > 0 ? ForwardOrientation : BackwardOrientation;
-
-		return new BoxSurfaceResult(portalPos, portalOrientation);
-	}
+	private Vector2 size;
+	private const float colliderThickness = 0.05f;
 
 	private void Awake() {
 		size = initialSize;
@@ -54,5 +30,21 @@ public class PortalSurface : MonoBehaviour
 		}
 		collider.size = new Vector3(initialSize.x, initialSize.y, colliderThickness);
 		collider.isTrigger = true;
+	}
+
+    public BoxSurfaceResult GetPortalPosit(Vector3 hitWorld, Vector2 portalSize) {
+		Vector3 hitLocal = transform.InverseTransformPoint(hitWorld);
+		portalSize /= new Vector2(transform.lossyScale.x, transform.lossyScale.y);
+		float x = Mathf.Clamp(hitLocal.x, -(size.x - portalSize.x)/2, (size.x - portalSize.x)/2);
+		float y = Mathf.Clamp(hitLocal.y, -(size.y - portalSize.y)/2, (size.y - portalSize.y)/2);
+		float z = hitLocal.z > 0 ? Offset : -Offset;
+		Vector3 portalPos = transform.TransformPoint(new Vector3(x, y, z));
+		Quaternion portalOrientation;
+        if (hitLocal.z > 0)
+            portalOrientation = Quaternion.LookRotation(transform.forward, Vector3.up);
+        else
+            portalOrientation = Quaternion.LookRotation(-transform.forward, Vector3.up);
+
+        return new BoxSurfaceResult(portalPos, portalOrientation);
 	}
 }
