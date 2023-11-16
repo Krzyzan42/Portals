9dc6b3e322d15982f1129d38151c8bf2816bff33
Krzyzan <jakubkrzyzanowski42@gmail.com>
2023-11-16 14:27:18 +0100

    Input changes

--- Assets/Scripts/Other/UserInput.cs
+++ Assets/Scripts/Other/UserInput.cs
@@ -5,5 +5,5 @@ public static class UserInput
 {
 	public static bool GrappleKeyDown => Input.GetMouseButtonDown(1);
 	public static bool GrappleKeyUp => Input.GetMouseButtonUp(1);
-	public static bool GrapplePullPressed => Input.GetKey(KeyCode.LeftShift);
+	public static bool GrapplePullPressed => Input.GetMouseButton(1);
 }
